8e6d3ce16b88f3e53384d6569af3781619402d62
Culture Bunker Recordings <culturebunkerrecordings@gmail.com>
2020-01-15 11:01:42 +0000

    first attempt

--- main.cpp
+++ main.cpp
@@ -32,8 +32,72 @@
  Wait for my code review.
  */
 
+struct FloatType
+{
+    float add( float lhs, float rhs );
+    float subtract( float lhs, float rhs );
+    float multiply( float lhs, float rhs );
+    float divide( float lhs, float rhs );
+};
+
+float FloatType::add( float lhs, float rhs )
+{
+    return  lhs + rhs;
+}
+
+float FloatType::subtract( float lhs, float rhs )
+{
+    return lhs - rhs;
+}
+ 
+ struct DoubleType 
+ {
+    double add( double lhs, double rhs );
+    double subtract( double lhs, double rhs );
+    double multiply( double lhs, double rhs );
+    double divide( double lhs, double rhs );
+ };
+
+ double DoubleType::multiply(double lhs, double rhs)
+ {
+     return lhs * rhs;
+ }
+ 
+ double DoubleType::subtract(double lhs, double rhs)
+ {
+     return lhs - rhs;
+ }
+
+ struct IntType
+ {
+    int add( int lhs, int rhs );
+    int subtract( int lhs, int rhs );
+    int multiply( int lhs, int rhs );
+    int divide( int lhs, int rhs );
+ };
+
+ int IntType::divide(int lhs, int rhs)
+ {
+     return lhs/rhs;
+ }
+
 #include <iostream>
 int main()
 {
+    FloatType f;
+    IntType i;
+    DoubleType d;
+    auto addF = f.add(2.5f, 6.5f);
+    auto subF = f.subtract(2.5f, 6.5f);
+    auto divI = i.divide(8, 2);
+    auto multiD = d.multiply(4.4567, 3.7899765);
+    auto subD = d.subtract(45.6784, 20.7895);
+
+    std::cout<< "result of f.add(): "<<addF<<std::endl;
+    std::cout<< "result of f.subtract(): "<<subF<<std::endl;
+    std::cout<< "result of i.divide(): "<<divI<<std::endl;
+    std::cout<< "result of d.multiply(): "<<multiD<<std::endl;
+    std::cout<< "result of d.subtract(): "<<subD<<std::endl;
+
     std::cout << "good to go!" << std::endl;
 }
